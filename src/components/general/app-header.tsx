922c7175b649f387446c24a0cf32634c2393a586
mykhailo <m.diakovych13@gmail.com>
2025-02-04 15:01:58 +0200

    refactor: lint fix

--- src/components/general/app-header.tsx
+++ src/components/general/app-header.tsx
@@ -1,11 +1,7 @@
-import { Button } from "../ui/button";
-
 export function AppHeader() {
-  return (
-    <div className="h-[60px] border-b border-v-grey-200 justify-end flex items-center px-8 py-2.5">
-      <div className="flex items-center gap-2">
-        btn
+   return (
+      <div className="flex h-[60px] items-center justify-end border-b border-v-grey-200 px-8 py-2.5">
+         <div className="flex items-center gap-2">btn</div>
       </div>
-    </div>
-  );
+   );
 }
--- src/components/general/logo/Logo.tsx
+++ src/components/general/logo/Logo.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 
 export const Logo = () => {
-    return (
-        <div>
-            <span className='text-xl font-normal leading-6 text-left'>VOLTQUANT</span>
-        </div>
-    );
+   return (
+      <div>
+         <span className="text-left text-xl font-normal leading-6">
+            VOLTQUANT
+         </span>
+      </div>
+   );
 };
