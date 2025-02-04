90520c27405a398874796efae2d6d693579c6ade
mykhailo <m.diakovych13@gmail.com>
2025-02-04 13:17:14 +0200

    feat: header section

--- src/components/general/app-header.tsx
+++ src/components/general/app-header.tsx
@@ -1,5 +1,11 @@
+import { Button } from "../ui/button";
+
 export function AppHeader() {
   return (
-    <div>HEADER</div>
+    <div className="h-[60px] border-b border-v-grey-200 justify-end flex items-center px-8 py-2.5">
+      <div className="flex items-center gap-2">
+        btn
+      </div>
+    </div>
   );
 }
